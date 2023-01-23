946c48762adc47d37614c90f4cb2d9dddfbc008b
Brittany Laura Catone <bcatone@outlook.com>
2023-01-23 17:11:09 -0500

    initial components setup

--- my-app/my-app-frontend/src/components/App.js
+++ my-app/my-app-frontend/src/components/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import React from 'react';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
-      
+      <Login />
     </div>
   );
 }
---
+++ my-app/my-app-frontend/src/components/LoginForm.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+function LoginForm() {
+    // const [userInfo, setUserInfo] = useState({
+    //     username: "",
+    //     password: ""
+    // });
+
+    // return (
+        
+    // );
+};
+
+export default LoginForm
